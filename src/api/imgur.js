95b7c5a11cbf73eb21b0f9bdf1a401ca58b4a973
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-04-22 20:43:49 +0500

    create fetchImages helper function to make get requests to imgur api

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from 'qs';
+import axios from 'axios';
 const CLIENT_ID = '4319c513f3f4e5c';
 const ROOT_URL = 'https://api.imgur.com';
 
@@ -12,5 +13,13 @@ export default {
     window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(
       queryString
     )}`;
+  },
+
+  fetchImages(token) {
+    return axios.get(`${ROOT_URL}/3/account/me/images`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   },
 };
