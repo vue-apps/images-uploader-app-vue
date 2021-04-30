a1e571f055ec0cf2dc0d8c4588b4bc50db161f5b
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-05-01 01:12:15 +0500

    temporarily render fetched images from imgur api in ImageList component

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,16 +1,24 @@
 <template>
-  <div>
-    ImageList
+  <div class="ui container">
+    <img v-for="image in allImages" :key="image.id" :src="image.link" alt="" />
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 export default {
   name: 'ImageList',
+  computed: mapGetters(['allImages']),
   methods: mapActions(['fetchImages']),
   created() {
     this.fetchImages();
   },
 };
 </script>
+
+<style scoped>
+img {
+  width: 200px;
+  padding: 10px;
+}
+</style>
