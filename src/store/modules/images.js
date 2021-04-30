6841eaca2d5fbaef4f0d5665442b1cdf4936ee07
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-05-01 00:40:47 +0500

    commit retrieved images from imgur api endpoint to the state of images module

--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -10,10 +10,10 @@ export default {
   },
 
   actions: {
-    async fetchImages({ rootState }) {
+    async fetchImages({ rootState, commit }) {
       const { token } = rootState.auth;
       const response = await api.fetchImages(token);
-      console.log(response);
+      commit('setImages', response.data.data);
     },
   },
 
